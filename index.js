ae3b741dee6fdb3dc3f19f40cde3512a51372799
Garrett <hawkpdx@icloud.com>
2024-11-24 20:15:15 -0800

    wrapAdjective digested... relatively

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+function saturdayAlt(activity = `bathe my dog`) {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayAlt());
+function mondayWork(activity = `go to the office`) {
+  return `This Monday, I will ${activity}.`;
+}
+
+function mondayAlt(activity = `work from home`) {
+  return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(flair = "*") {
+  return function (adjective = "a hard worker") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+function wrapAdjectiveAlt(flair = "||") {
+  return function (adjective = "a dedicated programmer") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+console.log(wrapAdjectiveAlt(flair));
